4baadcf8a21cce5d9921c034482e152e2118f2c1
Tristan Galloway <tristan.a.galloway@gmail.com>
2024-03-08 20:56:36 -0700

    Finished resume program.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";   
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Boeing";
+        job2._jobTitle = "Programmer";
+        job2._startYear = 2015;
+        job2._endYear = 2018;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Tristan Galloway";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
