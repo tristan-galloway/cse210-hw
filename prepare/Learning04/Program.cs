c240c807f1947ec669624238e0cf968421946080
Tristan Galloway <tristan.a.galloway@gmail.com>
2024-03-20 17:36:31 -0600

    finished prep assignment week04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment math = new("Tristan Galloway", "Math");
+
+        Console.WriteLine(math.GetSummary());
     }
 }
